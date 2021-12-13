bf3f4510a9f307e5719778d3391787a41f37ede2
Arthur <arthur15951@gmail.com>
2021-12-13 13:10:42 -0300

    ajeitei os arquivos

--- js/menu.js
+++ js/menu.js
@@ -27,3 +27,16 @@ document.querySelector("#subMenuMobileButton").addEventListener("click", functio
         subMenu.style.display = 'block';
     }
 });
+
+let key = document.querySelector('#keyColor');
+let downSvg = document.querySelector('#downSvg');
+let upSvg = document.querySelector('#upSvg');
+
+document.getElementById("keyColor").addEventListener("mouseenter", function( event ) {
+    downSvg.style.display = 'none';
+    upSvg.style.display = 'inline';
+});
+document.getElementById("keyColor").addEventListener("mouseleave", function( event ) {
+    downSvg.style.display = 'inline';
+    upSvg.style.display = 'none';
+});
